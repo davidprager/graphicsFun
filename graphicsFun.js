9b451e2e7947da92c65dbde738bd61061f1b1f75
davidprager <david_prager@yahoo.com>
2021-12-04 20:26:36 -0500

    Initial commit

---
+++ graphicsFun.js
@@ -0,0 +1,61 @@
+/*
+ * File: Graphics.js
+ * ---------------------------
+ * This file contains a series of functions to examine how the JavaScript Graphics library works
+ */
+
+"use strict";
+
+/* Constants */
+
+const GWINDOW_WIDTH = 500;
+const GWINDOW_HEIGHT = 300;
+const BALLOON_WIDTH = 140;
+const BALLOON_HEIGHT = 160;
+const BALLOON_LABEL = "JS is fun!";
+const CORD_LENGTH = 100;
+const CIRCLE_SIZE = 75;
+const CIRCLE_SEPARATION = 20;
+
+/* This function displays "Hello World"*/
+function drawIt() {
+
+}
+
+/* This function creates a blue rectangle  */
+/*function drawIt() {
+    let gw = GWindow(GWINDOW_WIDTH, GWINDOW_HEIGHT);
+
+}*/
+
+/* This function creates a blue rectangle with a red oval inside it */
+/*function drawIt() {
+
+}*/
+
+/* This function will draw the diagonal lines across the Graphics Window */
+/*function drawIt() {
+    let gw = GWindow(GWINDOW_WIDTH, GWINDOW_HEIGHT);
+
+}*/
+
+/* This function displays "Hello World" centered in the Graphics Window */
+/*function drawIt() {
+    let gw = GWindow(GWINDOW_WIDTH, GWINDOW_HEIGHT);
+
+}*/
+
+/* This function displays a red balloon with text on it */
+/*function drawIt() {
+
+}*/
+
+/* This function draws five circles equally spaced across the graphics window */
+/*
+function drawIt() {
+    let gw = GWindow(GWINDOW_WIDTH, GWINDOW_HEIGHT);
+
+}
+*/
+
+
